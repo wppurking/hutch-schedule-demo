e3c55581029dcf4f84c93fe1d758a4893c1d4fbe
wyatt <wppurking@gmail.com>
2017-05-23 18:49:22 +0800

    * upgrade to hutch 0.4.2 * test hutch with channel_prefetch: 10

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.0'
 gem 'pry-rails'
-gem 'hutch-schedule', '~> 0.3'
+gem 'hutch-schedule', '~> 0.4'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
--- config/initializers/z-001-hutch.rb
+++ config/initializers/z-001-hutch.rb
@@ -1,5 +1,5 @@
 # load all activejob class
-puts Hutch::Config.load_from_file(Rails.root.join('config', 'config.yaml'))
+puts Hutch::Config.load_from_file(Rails.root.join('config', 'hutch.yaml'))
 Hutch::Config.error_handlers = [Hutch::ErrorHandlers::MaxRetry.new]
 Hutch.connect
 Hutch::Schedule.connect
