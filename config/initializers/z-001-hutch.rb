2023dccb9c7ba74b515bcba62415c3bd0f7022ef
wyatt <wppurking@gmail.com>
2020-04-15 15:43:28 +0800

    * 解决 hutch 在 production 环境运行的问题

--- config/initializers/z-001-hutch.rb
+++ config/initializers/z-001-hutch.rb
@@ -1,4 +1,5 @@
 # load all activejob class
 puts "hutch.yaml: #{Hutch::Config.load_from_file(Rails.root.join('config', 'hutch.yaml'))}"
 Hutch::Config.error_handlers = [Hutch::ErrorHandlers::MaxRetry.new]
+Hutch::Config.log_level = Logger::WARN
 Hutch::Schedule.connect
